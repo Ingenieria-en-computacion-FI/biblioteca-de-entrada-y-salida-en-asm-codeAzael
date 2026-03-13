ade5772cf015e184591b9ed628fd568166dda6c9
Azael <azaelmart1010@gmail.com>
2026-03-13 11:46:47 -0600

    Cambio a main para que funcione correctamente

--- src/main.asm
+++ src/main.asm
@@ -3,30 +3,27 @@
 global _start
 
 SECTION .data
+    msg_int    db "Ingrese un numero: ", 0
+    msg_char   db "Ingrese un caracter: ", 0
+    msg_str    db "Ingrese una cadena: ", 0
+    msg_result db 10, "Resultados:", 10, 0
 
-msg_int db "Ingrese un numero:",10,0
-msg_char db "Ingrese un caracter:",10,0
-msg_str db "Ingrese una cadena:",10,0
 
-msg_result db "Resultados:",10,0
-
-buffer resb 64
+SECTION .bss
+    var_int    resd 1    ; guardar el número (4 bytes)
+    var_char   resb 1    ; guardar el carácter (1 byte)
+    var_str    resb 64   ; guardar la cadena (64 bytes)
 
 SECTION .text
-
 _start:
-
     ; -------------------------
     ; pedir entero
     ; -------------------------
 
     mov eax, msg_int
     call print_string
-
     call scan_int
-    call print_int
-    call newline
-
+    mov [var_int], eax   ; guardamos lo que regresó EAX
 
     ; -------------------------
     ; pedir caracter
@@ -34,11 +31,10 @@ _start:
 
     mov eax, msg_char
     call print_string
-
     call scan_char
-    call print_char
-    call newline
-
+    mov [var_char], al   ; guardamos el byte en la variable
+    
+    call scan_char 
 
     ; -------------------------
     ; pedir cadena
@@ -46,17 +42,48 @@ _start:
 
     mov eax, msg_str
     call print_string
-
-    mov eax, buffer
+    mov eax, var_str     ; Le pasamos la dirección de nuestra variable
     mov ebx, 64
     call scan_string
+
+    ; -----------------------------------------
+    ; MOSTRAR TODO AL FINAL
+    ; -----------------------------------------
 
     mov eax, msg_result
     call print_string
 
-    mov eax, buffer
+    ; -------------------------
+    ; imprimir entero
+    ; -------------------------
+
+    mov eax, [var_int]
+    call print_int
+    call newline
+
+    ; -------------------------
+    ; imprimir caracter
+    ; -------------------------
+
+    mov al, [var_char]
+    call print_char
+    call newline
+
+    ; -------------------------
+    ; imprimir cadena
+    ; -------------------------
+
+    mov eax, var_str
     call print_string
     call newline
+
+    ; -------------------------
+    ; salir
+    ; -------------------------
+
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
 
 
     ; -------------------------
