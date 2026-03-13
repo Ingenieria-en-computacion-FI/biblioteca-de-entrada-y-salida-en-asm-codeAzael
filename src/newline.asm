6c5df5964cf2457678786af94f0541f2f642424d
Azael <azaelmart1010@gmail.com>
2026-03-13 11:47:25 -0600

    Implementacion de la funcion newline

--- src/newline.asm
+++ src/newline.asm
@@ -4,13 +4,24 @@ global newline
 SECTION .text
 
 newline:
+    ; --- Prólogo de la función ---
+    push ebp                ; Guardar el puntero de base
+    mov ebp, esp            ; Establecer el nuevo puntero de base
 
-    push ebp
-    mov ebp, esp
+    ; --- Preservar registros ---
+    push eax                ; Guardamos eax para no perder su valor original
 
     ; TODO:
     ; imprimir '\n'
+    ; --- 1. Preparar el carácter de salto de línea ---
+    mov al, 10              ; Cargar el código ASCII 10 en el registro AL
 
-    mov esp, ebp
-    pop ebp
-    ret
+    ; --- 2. Llamar a la función de impresión ---
+    call print_char
+
+    ; --- Restaurar registros y Epílogo ---
+    pop eax                 ; Recuperar el valor original de eax
+
+    mov esp, ebp            ; Restaurar el puntero de pila
+    pop ebp                 ; Restaurar el puntero de base
+    ret                     ; Retornar al procedimiento que llamó
