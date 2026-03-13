ed7cfb67ad31317d0f30d61015f8ec33f52e1a40
Azael <azaelmart1010@gmail.com>
2026-03-13 11:49:14 -0600

    Implementacion de la funcion print_string

--- src/print_string.asm
+++ src/print_string.asm
@@ -9,14 +9,46 @@ global print_string
 ; ---------------------------------
 
 print_string:
+    ; --- Prólogo de la función ---
+    push ebp                        ; Guardar el puntero de base
+    mov ebp, esp                    ; Establecer el nuevo puntero de base
 
-    push ebp
-    mov ebp, esp
+    ; --- Preservar registros que serán utilizados ---
+    push ebx                        ; ebx se usa para el File Descriptor en el syscall
+    push esi                        ; esi se usa como puntero para recorrer la cadena
 
     ; TODO:
     ; 1. calcular longitud
-    ; 2. syscall write
+    mov esi, eax                    ; esi apunta al inicio de la cadena (desde EAX)
+    mov ecx, eax                    ; ecx guarda la dirección original para el syscall write
+    xor edx, edx                    ; Inicializar edx en 0 (será nuestro contador de longitud)
 
+calcular_len:
+    ; Comparamos el byte actual con el terminador nulo (0)
+    cmp byte [esi + edx], 0
+    je string_imprimir              ; Si encontramos el 0, saltamos a imprimir
+    inc edx                         ; Si no es 0, incrementamos el contador de bytes
+    jmp calcular_len
+    ; 2. syscall write
+string_imprimir:
+    ; Si la longitud es 0 (cadena vacía), saltamos al final para evitar errores
+    cmp edx, 0
+    je ps_fin
+
+    ; Configuración para sys_write (int 0x80)
+    ; eax = 4 (ID de sys_write)
+    ; ebx = 1 (stdout)
+    ; ecx = Dirección de la cadena (ya cargada previamente)
+    ; edx = Cantidad de bytes a escribir (calculada en el paso anterior)
+    mov eax, 4
+    mov ebx, 1
+    int 0x80
+
+ps_fin:
+    ; --- Restaurar registros y Epílogo ---
+    pop esi
+    pop ebx
+    
     mov esp, ebp
     pop ebp
     ret
